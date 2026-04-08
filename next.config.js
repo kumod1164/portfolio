41fc6198db2393009dad3483008bae097994d6e5
kumod1164 <kumodsharma1164@gmail.com>
2026-04-08 23:18:00 +0530

    deployment fixes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
-  // If you use basePath or assetPrefix, set them here for GitHub Pages
-  // assetPrefix: process.env.NODE_ENV === 'production' ? '/kumod1164.github.io/' : '',
 };
 
 module.exports = nextConfig; 
